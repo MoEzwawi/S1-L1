6e1a6e600be9c310d3f59d6b5b8ace5f3dd4f6cb
MoEzwawi <mezwawi96@gmail.com>
2023-11-27 16:36:25 +0100

    finishes second exercise

--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
+        System.out.println("Please write three strings and press Enter after each time");
+        Scanner input = new Scanner(System.in);
+
+        String s1 = input.nextLine();
+        String s2 = input.nextLine();
+        String s3 = input.nextLine();
+
+        System.out.println("Direct order: " + s1 + " " + s2 + " " + s3);
+        System.out.println("Reverse order: " + s3 + " " + s2 + " " + s1);
     }
 }
